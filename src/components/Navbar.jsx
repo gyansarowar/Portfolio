bb211781ca18beee43764eded1f58cbc284378e2
Gyan Sarowar <gs.gyan.007@gmail.com>
2026-06-12 20:33:05 +0530

    Sidebar nav fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -393,7 +393,7 @@ const Navbar = () => {
       </div> */}
 
       {/*Social Icons */}
-      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+      <div className="hidden lg:flex fixed flex-col top-[35%] z-10 left-0">
         <ul>
           {/*LINKEDIN */}
           <li className=" w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 ">
